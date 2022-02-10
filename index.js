a56ce36ce105ba76b8080ba89ee1549dca7a5a12
bigtoe <xxxlata@naver.com>
2022-02-10 13:34:17 +0900

    clone

---
+++ index.js
@@ -0,0 +1,8 @@
+const menu= document.querySelector(".click-menu i");
+document.querySelector(".click-search");
+
+function clickMenu () {
+    menu.style.color ="blue";
+}
+
+menu.addEventListener("click",clickMenu);
